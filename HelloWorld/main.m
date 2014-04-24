c80fe9aa9c395954468e071895c5c8f4c7b2ca76
Dave Wieringa <dwjm@userwise.com>
2014-04-24 18:11:03 -0400

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by David Wieringa on 4/24/14.
+//  Copyright (c) 2014 Userwise Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
